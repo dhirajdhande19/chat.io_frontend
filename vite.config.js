768996954f61063f9856f86aa82e103971f7d80e
dhirajdhande19 <dhirajdhande2@gmail.com>
2026-01-16 16:19:32 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    hmr: true, // ensures HMR is enabled
-  },
-
+  // server: {
+  //   hmr: true, // ensures HMR is enabled
+  // },
 });
